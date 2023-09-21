f57c9f9dc449d8254693f81d715576c185944041
elain <elain@Elaines_Laptop.dhcp.wustl.edu>
2023-09-21 12:47:11 -0500

    as of thursday 9/21

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -10,20 +10,27 @@ public class Sieve {
 		System.out.println("All prime nunbers up to which number?");
 		int n = in.nextInt();
 		boolean [] sieve_items = new boolean[n];
+		//System.out.println(sieve_items);
 for (int i=2; i<sieve_items.length; i++)
 {
 	sieve_items [i] = true;
-	System.out.println(sieve_items[i]);
+	System.out.println(sieve_items[i]);//test print
 	
-	for (int j=2; j<sieve_items.length; j++)
-		if (sieve_items[j] = true)
-	{
-	{
-		sieve_items [i*j]= false;
-		System.out.println(sieve_items[j]);
-	}
+	for (int j=2; j<sieve_items.length; j++) {
+		i=j;
+		i=i+j;
+		sieve_items[i]=false;
+			//[j] = true;
+			//System.out.println(j);//test print
+		}
+		//else
+		//{
+		//sieve_items [i*j]= false;
+		//System.out.println(sieve_items[j]);
+	//}
 }
 }
-	}
+}
 
-}
+
+
---
+++ src/studio3/Ssieve.java
@@ -0,0 +1,10 @@
+package studio3;
+
+public class Ssieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
